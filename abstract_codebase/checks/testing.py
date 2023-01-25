f272d17b918238d7169ac9c4bfe79ebff8c5ebbc
Peter Hartog <peter.hartog@hotmail.nl>
2023-01-25 17:43:37 +0000

    Added basic testing for testing observer

---
+++ abstract_codebase/checks/testing.py
@@ -0,0 +1,35 @@
+from typing import Any, Callable
+import warnings
+from abstract_codebase.patterns.observer import RegistryObserver
+
+
+class Testing(RegistryObserver):
+    """A testing observer."""
+
+    def __init__(self, test_module: Callable, forced: bool = False):
+        super().__init__()
+        self.forced = forced
+        self.test_module = test_module
+
+    def register_event(self, key: str, object: Any, **kwargs):
+        try:
+            self.test_module(object)
+            self.index[key] = True
+        except Exception:
+            if self.forced:
+                raise AssertionError("Object must pass the test module.")
+            else:
+                warnings.warn("Object must pass the test module.")
+                self.index[key] = False
+
+    def call_event(self, key: str, **kwargs):
+        pass
+
+    def get_info(self, key: str) -> Any:
+        return self.index[key]
+
+    def info(self, key: str) -> str:
+        if self.index[key] is True:
+            return f"{key} has passed testing."
+        else:
+            return f"{key} has not passed testing."
---
+++ tests/test_testing_check.py
@@ -0,0 +1,103 @@
+"""Test cases for Registry factory pattern ensurance.
+Author: PeterHartog
+"""
+import pytest
+from abstract_codebase.checks.testing import Testing
+from abstract_codebase.factory import Factory
+
+
+class CallableTestModule:
+    """Module to test."""
+
+    def __init__(self, name):
+        self.name = name
+        self.assert_name()
+
+    def assert_name(self):
+        assert self.name == "test", "Name is not test"
+
+
+class TestFactoryPattern:
+    """Test cases for versioning."""
+
+    class TestFactory(Factory):
+        TestRegistry = Factory.create_registry(
+            shared=False, checks=[Testing(test_module=CallableTestModule, forced=False)]
+        )
+        TestSharedRegistry = Factory.create_registry(
+            shared=True, checks=[Testing(test_module=CallableTestModule, forced=False)]
+        )
+        ForcedRegistry = Factory.create_registry(
+            shared=False, checks=[Testing(test_module=CallableTestModule, forced=True)]
+        )
+        ForcedSharedRegistry = Factory.create_registry(
+            shared=True, checks=[Testing(test_module=CallableTestModule, forced=True)]
+        )
+
+    def test_testing(self):
+        """Test the testing."""
+
+        self.TestFactory.TestRegistry.register_prebuilt(key="name_test", obj="test")
+
+    def test_wrong_pattern(self):
+        """Test the wrong pattern."""
+
+        with pytest.warns():
+            self.TestFactory.TestRegistry.register_prebuilt(key="wrong_name_test", obj="not_test")
+
+        with pytest.raises(Exception):
+            self.TestFactory.ForcedRegistry.register_prebuilt(key="wrong_name_test", obj="not_test")
+
+    # def test_inhereted_pattern(self):
+    #     """Test inhereted pattern."""
+
+    #     class InheretedPattern(Pattern):
+    #         pass
+
+    #     class DoubleInheretedPattern(InheretedPattern):
+    #         pass
+
+    #     self.TestFactory.TestRegistry.register_prebuilt(key="inhereted_test", obj=InheretedPattern)
+    #     self.TestFactory.TestRegistry.register_prebuilt(key="double_inhereted_test", obj=DoubleInheretedPattern)
+
+    # def test_wrong_inhereted_pattern(self):
+    #     """Test wrong inhereted pattern."""
+
+    #     class WrongPattern:
+    #         pass
+
+    #     with pytest.warns():
+    #         self.TestFactory.TestRegistry.register_prebuilt(key="wrong_inhereted_test", obj=WrongPattern)
+
+    #     with pytest.raises(Exception):
+    #         self.TestFactory.ForcedRegistry.register_prebuilt(key="wrong_inhereted_test", obj=WrongPattern)
+
+    # def test_common_pattern(self):  # TODO: This shouldn't be a warning, but it is.
+    #     """Test common pattern."""
+
+    #     class CommonPattern:
+    #         def __init__(self, name):
+    #             self.name = name
+
+    #         def hello_world(self):
+    #             """Hello world."""
+    #             print("Hello world")
+
+    #     self.TestFactory.TestRegistry.register_prebuilt(key="common_pattern_test", obj=CommonPattern)
+
+    # def test_wrong_common_pattern(self):
+    #     """Test wrong common pattern."""
+
+    #     class WrongPattern:
+    #         def __init__(self, name):
+    #             self.name = name
+
+    #         def hello_world2(self):
+    #             """Hello world 2."""
+    #             print("Hello world 2: Electric Boogaloo")
+
+    #     with pytest.warns():
+    #         self.TestFactory.TestRegistry.register_prebuilt(key="wrong_common_pattern_test", obj=WrongPattern)
+
+    #     with pytest.raises(Exception):
+    #         self.TestFactory.ForcedRegistry.register_prebuilt(key="wrong_common_pattern_test", obj=WrongPattern)
